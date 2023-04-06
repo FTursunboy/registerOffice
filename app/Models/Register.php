ac913ea1e856eba591cb9199037f26bd873d038c
Nuriddin Shahobov <100368507+nuriddinshahobov2001@users.noreply.github.com>
2023-04-06 16:10:09 +0500

    asd

--- app/Models/Register.php
+++ app/Models/Register.php
@@ -5,9 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class register extends Model
+class Register extends Model
 {
     use HasFactory;
-    
+
     protected $fillable = ['name', 'manager', 'zam-director', 'otdel_id', 'phone', 'sec_phone', 'email', 'index', 'region', 'village', 'street', 'street_number', 'status'];
 }
